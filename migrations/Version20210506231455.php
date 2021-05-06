2409867e3917f1ec0a2a80e3f35a626dbfa0bdb8
Reid Matthews <reid.matthews@gmail.com>
2021-05-06 19:21:32 -0400

    Fix Doctrine migration.

---
+++ migrations/Version20210506231455.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210506231455 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE county_month_data (case_month VARCHAR(7) NOT NULL, res_county VARCHAR(255) NOT NULL, total_cases INT NOT NULL, stats TEXT NOT NULL, PRIMARY KEY(case_month, res_county))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE county_month_data');
+    }
+}
